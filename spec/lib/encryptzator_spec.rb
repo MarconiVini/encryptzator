36d6f844949ce58adcb187ff1d3d481d31d1bd2f
Marcos Santini <cardosounicamp@gmail.com>
2015-02-05 22:51:26 -0200

    refactored specs to be more concise

--- spec/lib/encryptzator_spec.rb
+++ spec/lib/encryptzator_spec.rb
@@ -1,77 +1,62 @@
 require 'encryptzator'
 
 describe Encryptzator do 
-  describe 'ClassInitialize' do
-    it 'should initialize' do
-      u = Encryptzator.new
-      expect(u.class).to eq Encryptzator
-    end
+  let(:encryp)  { Encryptzator.new }
+  let(:message) { "Believe you can and you're halfway there." }
+  let(:my_pass) { "mg@&$$Hdhahnntrumble" }
 
-    describe 'Encrypting' do
-      before :each do
-        @encryp = Encryptzator.new
-        @message = "Believe you can and you're halfway there."
-        @my_pass = "mg@&$$Hdhahnntrumble"
-      end
+  it 'should initialize' do
+    expect(Encryptzator.new.class).to eq Encryptzator
+  end
 
-      it 'should have an encrypt message return values' do
-        message = @encryp.encrypt_message(@message, @my_pass)
-        expect(message[:encrypted_message]).not_to be nil
-        expect(message[:encrypted_message].encoding.to_s).to eq "ASCII-8BIT"
-        expect(message[:iv]).not_to be nil
-      end
-    end
-
-    describe 'Decrypting' do
-      before :each do 
-
-        @encryp = Encryptzator.new
-        @message = "Believe you can and you're halfway there."
-        @my_pass = "mg@&$$Hdhahnntrumble"
-        message = @encryp.encrypt_message(@message, @my_pass)
-        @iv = message[:iv]
-        @encrypted_message = message[:encrypted_message]
-        #hash for decrypting
-        @hash = { random_pass: @my_pass, iv: @iv, encrypted_message: @encrypted_message}
-      end
-
-      it "should check hash paramters integrity of random_pass" do
-        hash = {}
-        hash[:iv] = @iv
-        hash[:encrypted_message] = @encrypted_message
-        expect(lambda{@encryp.decrypt_message hash}).to raise_exception
-      end
-      
-      it "should check hash paramters integrity of iv" do
-        hash = {}
-        hash[:encrypted_message] = @encrypted_message
-        hash[:random_pass] = @my_pass
-        expect(lambda{@encryp.decrypt_message hash}).to raise_exception
-      end
-
-      it "should check hash paramters integrity of encrypted_message" do
-        hash = {}
-        hash[:iv] = @iv
-        hash[:random_pass] = @my_pass
-        expect(lambda{@encryp.decrypt_message hash}).to raise_exception
-      end
-
-      it "should decrypt the final message" do
-        expect(@encryp.decrypt_message @hash).to be_eql @message
-      end
-    end
-
-    describe 'Decrypting when message is in UTF-8' do
-      before :each do 
-        @encryp = Encryptzator.new
-        @message = "Believe you can and you're halfway there."
-        @my_pass = "mg@&$$Hdhahnntrumble"
-        @encrypt_message = @encryp.encrypt_message_utf8(@message, @my_pass)
-      end
-
-      it "should decrypt message" do
-        expect(@encryp.decrypt_message_utf8(@encrypt_message, @my_pass)).to be_eql @message
-      end
+  describe 'Encrypting' do
+    it 'should have an encrypt message return values' do
+      message_hash = encryp.encrypt_message(message, my_pass)
+      expect(message_hash[:encrypted_message]).not_to be nil
+      expect(message_hash[:encrypted_message].encoding.to_s).to eq "ASCII-8BIT"
+      expect(message_hash[:iv]).not_to be nil
     end
   end
+
+  describe 'Decrypting' do
+    let(:message_hash) { encryp.encrypt_message(message, my_pass) }
+    let(:iv)      { message_hash[:iv] }
+    let(:encrypted_message) { message_hash[:encrypted_message] }
+    #hash for decrypting
+    let(:hash) {{ random_pass: my_pass, iv: iv, encrypted_message: encrypted_message}}
+
+    it "should check hash paramters integrity of random_pass" do
+      hash = {}
+      hash[:iv] = iv
+      hash[:encrypted_message] = encrypted_message
+      expect(lambda{@encryp.decrypt_message hash}).to raise_exception
+    end
+    
+    it "should check hash paramters integrity of iv" do
+      hash = {}
+      hash[:encrypted_message] = encrypted_message
+      hash[:random_pass] = my_pass
+      expect(lambda{encryp.decrypt_message hash}).to raise_exception
+    end
+
+    it "should check hash paramters integrity of encrypted_message" do
+      hash = {}
+      hash[:iv] = @iv
+      hash[:random_pass] = @my_pass
+      expect(lambda{@encryp.decrypt_message hash}).to raise_exception
+    end
+
+    it "should decrypt the final message" do
+        expect(encryp.decrypt_message hash).to be_eql message
+    end
+  end
+
+  describe 'Decrypting when message is in UTF-8' do
+    let(:encrypt_message) {encryp.encrypt_message_utf8(message, my_pass)}
+
+    it "should decrypt message" do
+      expect(encryp.decrypt_message_utf8(encrypt_message, my_pass)).to be_eql message
+    end
+  end
+  
 end
